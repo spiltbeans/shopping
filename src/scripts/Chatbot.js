b89189950e87bbe9c814ad5e5aa20dc4b0bb2f57
jaleelah <91485585+jaleelah@users.noreply.github.com>
2023-02-11 16:16:55 -0500

    experimenting with prompts

--- src/scripts/Chatbot.js
+++ src/scripts/Chatbot.js
@@ -1,27 +1,16 @@
-import axios from 'axios'
-let payload = {
-    "model": "text-davinci-003",
-    "prompt": "Say this is a test",
-    "max_tokens": 7,
-    "temperature": 0,
-    "top_p": 1,
-    "n": 1,
-    "stream": false,
-    "logprobs": null,
-    "stop": "\n"
-}
+import { Configuration, OpenAIApi } from "openai";
 
 const OpenAi = () => {
-    axios({
-        method: "POST",
-        url: "https://api.openai.com/v1/completions",
-        data: payload,
-        headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${process.env.REACT_APP_OPENAI_KEY}`
-        }
-    }).then(r => console.log(r))
-
+    const configuration = new Configuration(
+        {organization: "org-6eSQ1gEfyWI7VJzjTpGCfCOX", apiKey: process.env.REACT_APP_OPENAI_KEY}
+    );
+    const Openai = new OpenAIApi(configuration);
+    const response = Openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: "Validate my choice to buy a GRACE KARIN Womens Casual High Waist Pencil Pants with Bow-Knot Pockets for Work is a good idea.",
+        max_tokens: 320,
+        temperature: 1,
+      }).then(r=>console.log(r))
     return <></>
 }
 
