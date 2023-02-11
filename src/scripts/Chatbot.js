ac2af13258eaa11753ca02af935a5f22fde3994f
Eyas Valdez <eyasvaldez7@gmail.com>
2023-02-11 16:23:40 -0500

    small code cleanup

--- src/scripts/Chatbot.js
+++ src/scripts/Chatbot.js
@@ -1,16 +1,17 @@
 import { Configuration, OpenAIApi } from "openai";
 
 const OpenAi = () => {
-    const configuration = new Configuration(
-        {organization: "org-6eSQ1gEfyWI7VJzjTpGCfCOX", apiKey: process.env.REACT_APP_OPENAI_KEY}
-    );
-    const Openai = new OpenAIApi(configuration);
-    const response = Openai.createCompletion({
+    const configuration = new Configuration({
+        organization: process.env.REACT_APP_OPENAI_ORG, apiKey: process.env.REACT_APP_OPENAI_KEY 
+    })
+
+    const openai = new OpenAIApi(configuration)
+    openai.createCompletion({
         model: "text-davinci-003",
         prompt: "Validate my choice to buy a GRACE KARIN Womens Casual High Waist Pencil Pants with Bow-Knot Pockets for Work is a good idea.",
         max_tokens: 320,
         temperature: 1,
-      }).then(r=>console.log(r))
+    }).then(r => console.log(r))
     return <></>
 }
 
