c6959895cd10bccd7bbdf9bee52f170e8a0b6d54
Eyas Valdez <eyasvaldez7@gmail.com>
2023-02-11 21:19:17 -0500

    app needed react query boilerplate code

--- src/App.js
+++ src/App.js
@@ -2,14 +2,20 @@ import Header from './components/Header'
 import Options from './components/Options';
 import Power from './components/Power';
 import Chatbot from './scripts/Chatbot'
+import { QueryClient, QueryClientProvider } from '@tanstack/query-core';
+
 const App = () => {
+  const queryClient = new QueryClient()
+
   return (
-    <div className={'w-[270px] h-[200px]'}>
-      <Header/>
-      <Options/>
-      <Power/>
-      <Chatbot/>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div className={'w-[270px] h-[200px]'}>
+        <Header />
+        <Options />
+        <Power />
+        <Chatbot />
+      </div>
+    </QueryClientProvider>
 
   );
 }
