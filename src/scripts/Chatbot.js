c66f6e7994b738f5de1b4f1c9fde215eb71f7f9a
jaleelah <91485585+jaleelah@users.noreply.github.com>
2023-02-11 21:00:57 -0500

    setting up chatbot hook

--- src/scripts/Chatbot.js
+++ src/scripts/Chatbot.js
@@ -1,19 +1,51 @@
+import axios from "axios";
+import { useAtom } from "atom";
+import { useQuery } from "@tanstack/react-query";
 import { Configuration, OpenAIApi } from "openai";
+import { itemAtom, powerAtom, encourageAtom, restrainAtom } from "../state/State"
 
-const OpenAi = ({tone, goal, product}) => {
+
+const useOpenAIApi = (tone, goal, product) => {
     const configuration = new Configuration({
         organization: process.env.REACT_APP_OPENAI_ORG, apiKey: process.env.REACT_APP_OPENAI_KEY 
     })
 
-
     const openai = new OpenAIApi(configuration)
-    openai.createCompletion({
-        model: "text-davinci-003",
-        prompt: `Using a ${tone} and informal tone, ${goal} my choice to buy a ${product}`,
-        max_tokens: 156,
-        temperature: 1,
-    }).then(r => console.log(r))
-    return <></>
+
+    const query_f = async () => {
+        return await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: `Using a ${tone} and informal tone, ${goal} my choice to buy a ${product}`,
+            max_tokens: 156,
+            temperature: 0.9,
+        }).then(r => console.log(r)).catch(e => { console.log(e) })
+    }
+    return useQuery({
+        queryKey: ['advice'],
+        queryFn: query_f
+    })
+}
+
+const OpenAi = () => {
+    const [item, setItem] = useAtom(itemAtom)
+    const [tone, setTone] = useAtom(encourageAtom)
+    if(tone !== true) {
+        if(item !== "") {
+            const { response } = useOpenAIApi({tone: "judgemental", goal: "dissuade", product: item})
+        }
+        else {
+            const { response } = useOpenAIApi({tone: "judgemental", goal: "dissaude", product: "Apple Pencil Tips - 4 Pack"})
+        }
+    }
+    else {
+        if(item !== "") {
+            const { response } = useOpenAIApi({tone: "friendly", goal: "validate", product: item})
+        }
+        else {
+            const { response } = useOpenAIApi({tone: "friendly", goal: "validate", product: "Strathmore Spiral Sketch Book 9-Inch by 12-Inch,100-Sheet"})
+        }
+    }
+    return <>response</>
 }
 
 export default OpenAi
--- src/state/State.js
+++ src/state/State.js
@@ -3,3 +3,4 @@ import { atom } from "jotai";
 export const powerAtom = atom(true)
 export const encourageAtom = atom(true)
 export const restrainAtom = atom(true)
+export const itemAtom = atom("")
